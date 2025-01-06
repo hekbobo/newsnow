5243aa7da1dbdc6a5e13e0e60b473bd82affaace
zyb <zyb@aisheshou.com>
2025-01-06 15:58:45 +0800

    3

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,6 @@
 export function Footer() {
   return (
     <>
-      <a href={`${Homepage}/LICENCE`} target="_blank">MIT LICENCE</a>
       <span>
         <span>NewsNow © 2024 By </span>
         <a href={Author.url} target="_blank">
